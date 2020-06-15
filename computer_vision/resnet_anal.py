2c5f6041f9853fbd4605eebd7ddc6e1539557fec
agagtmdtlr <agagtmdtlr@naver.com>
2020-06-15 18:24:32 +0900

    factor analytic

--- computer_vision/resnet_anal.py
+++ computer_vision/resnet_anal.py
@@ -1,18 +1,19 @@
 from tensorflow.keras import applications
-
-resnet = applications.ResNet50(weights='imagenet')
-resnet.summary()
-
-json_string = resnet.to_json()
+import tensorflow as tf
+# resnet = applications.ResNet50(weights='imagenet')
+# resnet.summary()
+# json_string = resnet.to_json()
 
 
 # with open('../../data/computer_vision/model.json','w') as f :
 #     f.write(json_string)
 
 import json
-parse = json.loads(json_string)
+# parse = json.loads(json_string)
 # print(json.dumps(parse, indent=4, sort_keys=True))
 
-pp_json_string = json.dumps(parse, indent=4, sort_keys=True)
-with open('../../data/computer_vision/model.json','w') as f :
-    f.write(pp_json_string)
+# pp_json_string = json.dumps(parse, indent=4, sort_keys=True)
+# with open('../../data/computer_vision/model.json','w') as f :
+#     f.write(pp_json_string)
+
+print(tf.keras.__version__)
