61f2de298cf4cf95a038a020a1febc0238301aee
briankunkel9 <briankunkel9@gmail.com>
2020-06-23 20:19:42 +0900

    change

--- computer_vision/cv_main_pss.py
+++ computer_vision/cv_main_pss.py
@@ -1,7 +1,7 @@
 
 import os
 import tensorflow as tf
-from refrigerator_recipe.computer_vision.cv_model import ResNet,Own,img64NN
+from refrigerator_recipe.computer_vision.cv_model import ResNet,Own,img64NN,img224NN
 from refrigerator_recipe.computer_vision.cv_dataset import DataSet
 from refrigerator_recipe.computer_vision.cv_keras_model import keras_resnet50,keras_vgg16,keras_resnet152
 from tensorflow.python.keras import losses
@@ -17,14 +17,14 @@ if __name__ == '__main__':
             # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
             print(e)
     with tf.device('/GPU:0'):
-        inputs = (64,64,3)
+        inputs = (224,224,3)
         outputs = 144
-        epochs = 200
+        epochs = 500
         batchs = 500
         opt = 'rmsprop'
         # 같은 모델이라도 옵션이 다를수 있는 부가적인 이름을 추가해쥇요
-        modelname_detail = 'extraction_64'
-        dataset_version = '_extraction_64'
+        modelname_detail = 'extraction'
+        dataset_version = '_extraction'
 
         dataset = DataSet(inputs,outputs)
         print(' ############모델 선택하기##################\n'
@@ -34,6 +34,7 @@ if __name__ == '__main__':
               ' VGG16 : v \n'
               ' Keras152 : 152\n'
               ' img64 : 64\n'
+              ' img224 : 224\n'
               ' .... : \n')
         command_key = input('키를 입력하세요( 대소문자 상관없음 ) : ').lower()
 
@@ -57,6 +58,9 @@ if __name__ == '__main__':
         elif command_key in ('64'):
             model = img64NN(inputs,outputs)
             modelname = f'img64_{modelname_detail}'
+        elif command_key in ('224'):
+            model = img224NN(inputs,outputs)
+            modelname = f'img224_{modelname_detail}'
 
 
         earlystop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=1)
@@ -85,20 +89,20 @@ if __name__ == '__main__':
 
 
         train_dataset = dataset.tfrecord_dataset(f'../../data/computer_vision_data/train{dataset_version}.tfrecord')
-        train_dataset = train_dataset.shuffle(buffer_size=512)
+        train_dataset = train_dataset.shuffle(buffer_size=20000).batch(batchs)
         valid_dataset = dataset.tfrecord_dataset(f'../../data/computer_vision_data/valid{dataset_version}.tfrecord')
-        valid_dataset = valid_dataset.shuffle(buffer_size=512)
+        valid_dataset = valid_dataset.shuffle(buffer_size=20000).batch(batchs)
         print('fitting 중입니다.')
-        model.fit(train_dataset, epochs=epochs, batch_size=batchs, verbose=1,validation_data=valid_dataset)
+        model.fit(train_dataset, epochs=epochs,verbose=1,validation_data=valid_dataset)
 
         model.save(model_path)
 
-        test_dataset = dataset.tfrecord_dataset(f'../../data/computer_vision_data/test{dataset_version}.tfrecord')
-        # test_dataset = test_dataset.shuffle(buffer_size=500)
-        print('evaluate 중입니다.')
-        test_loss, test_acc, test_top_k, test_cate_cross = model.evaluate(test_dataset, batch_size=batchs, verbose=1)
-        print('test_acc : %.4f' % test_acc)
-        print('test_loss : %.4f' % test_loss)
-        print('test_top_k : %.4f' % test_top_k)
-        print('test_categoricat_crossentropy : %.4f' % test_cate_cross)
-        print('-' * 50)
+        # test_dataset = dataset.tfrecord_dataset(f'../../data/computer_vision_data/test{dataset_version}.tfrecord')
+        # test_dataset = test_dataset.shuffle(buffer_size=20000)
+        # print('evaluate 중입니다.')
+        # test_loss, test_acc, test_top_k, test_cate_cross = model.evaluate(test_dataset, batch_size=batchs, verbose=1)
+        # print('test_acc : %.4f' % test_acc)
+        # print('test_loss : %.4f' % test_loss)
+        # print('test_top_k : %.4f' % test_top_k)
+        # print('test_categoricat_crossentropy : %.4f' % test_cate_cross)
+        # print('-' * 50)
--- computer_vision/cv_model.py
+++ computer_vision/cv_model.py
@@ -212,17 +212,21 @@ def ResNet(inputs,outputs):
     input_tensor = Input(shape=(x, y, z), dtype='float32', name='input')
 
     x = conv1_layer(input_tensor)
+    x = Dropout(0.2)(x)
     x = conv2_layer(x)
+    x = Dropout(0.2)(x)
     x = conv3_layer(x)
+    x = Dropout(0.2)(x)
     x = conv4_layer(x)
+    x = Dropout(0.2)(x)
     x = conv5_layer(x)
 
     x = GlobalAveragePooling2D()(x)
 
     x = Dense(1024, activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
-    x = Dropout(0.5)(x)
+    x = Dropout(0.2)(x)
     x = Dense(512, activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
-    x = Dropout(0.5)(x)
+    x = Dropout(0.2)(x)
     outputs_tensor= Dense(outputs,activation='softmax')(x)
     resnet50 = Model(input_tensor, outputs_tensor)
     resnet50.summary()
@@ -269,20 +273,14 @@ def Own(inputs,outputs):
 def img64NN(inputs,outputs):
     x, y, z = inputs
     model = Sequential()
-    # convolution layer
-    # padding = 'valid' (행열수 줄어듬), 'same' ( 행열수 보존 )
-    # filter 개수는 보통 32,64...
-    # Conv2D : input_shape -> ( 높이,너비,채널수 )
-    # 채널수는 흑백 : 1  컬럼 : 3(RGB)
-    # strides = (1,1) default
-    # convolution도 여러개로
+
     model.add(
         Conv2D(filters=64, kernel_size=(5, 5),strides=(1,1),
                activation='relu', input_shape=(x, y, z),
                padding='same',use_bias=True))
     model.add(MaxPooling2D(pool_size=(2, 2),strides=(2,2),
                            padding='same'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
 
     model.add(
         Conv2D(filters=64, kernel_size=(3, 3),strides=(1,1),
@@ -290,25 +288,25 @@ def img64NN(inputs,outputs):
                padding='same',use_bias=True))
     model.add(MaxPooling2D(pool_size=(2, 2),strides=(2,2),
                            padding='same'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L3
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L4
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L5
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
                            padding='same'))
 
@@ -317,8 +315,8 @@ def img64NN(inputs,outputs):
 
     # full connected
     model.add(Dense(units=384, activation=activations.relu,
-                    kernel_regularizer=tf.keras.regularizers.l2(0.001),use_bias=True))
-    model.add(Dropout(0.5))
+                    use_bias=True))
+    model.add(Dropout(0.2))
     model.add(Dense(units=outputs, activation=activations.softmax))
 
     model.summary()
@@ -341,7 +339,7 @@ def img224NN(inputs,outputs):
                padding='same',use_bias=True))
     model.add(MaxPooling2D(pool_size=(2, 2),strides=(2,2),
                            padding='same'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
 
     model.add(
         Conv2D(filters=64, kernel_size=(3, 3),strides=(1,1),
@@ -349,25 +347,25 @@ def img224NN(inputs,outputs):
                padding='same',use_bias=True))
     model.add(MaxPooling2D(pool_size=(2, 2),strides=(2,2),
                            padding='same'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L3
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L4
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     # L5
     model.add(
         Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1),
                activation='relu',
                padding='same', use_bias=True))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
                            padding='same'))
 
@@ -377,10 +375,10 @@ def img224NN(inputs,outputs):
     # full connected
     model.add(Dense(units=384, activation=activations.relu,
                     kernel_regularizer=tf.keras.regularizers.l2(0.001),use_bias=True))
-    model.add(Dropout(0.5))
+    # model.add(Dropout(0.5))
     model.add(Dense(units=192, activation=activations.relu,
-                    kernel_regularizer=tf.keras.regularizers.l2(0.001), use_bias=True))
-    model.add(Dropout(0.5))
+                    use_bias=True))
+    # model.add(Dropout(0.5))
     model.add(Dense(units=outputs, activation=activations.softmax))
 
     model.summary()
