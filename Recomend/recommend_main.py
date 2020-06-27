40c0910287bb82926a765dc8e61cec0aa926534d
briankunkel9 <briankunkel9@gmail.com>
2020-06-27 23:40:38 +0900

    draw graph recommend recipe

--- Recomend/recommend_main.py
+++ Recomend/recommend_main.py
@@ -1,11 +1,14 @@
 import os
 import cv2
 import pickle
+import time
 import math
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+
 
 from tensorflow.python.keras.preprocessing.image import load_img,img_to_array
 from refrigerator_recipe.computer_vision.cv_predict import Prediction
@@ -60,7 +63,7 @@ if __name__ == '__main__':
     # img_array = img_array.reshape(1,224,224,3)
     # prediction.predict_label(img_array)
 
-
+    start_time = time.time()
     ############# 판별 이미지로 레시피 검색 #############
     source_path = '../../data/nlp_data/source_embedding.csv'
     recipe_path = '../../data/nlp_data/recipe_embedding.csv'
@@ -68,7 +71,7 @@ if __name__ == '__main__':
     recipeFrame = pd.read_csv(recipe_path)
     print(sourceFrame.shape,'\n',recipeFrame.shape)
 
-    sources = ['버터']
+    sources = ['참치','고추']
     select_cat = ('볶음','일상',None,None)
 
     my_point = find_point(sources)
@@ -104,25 +107,53 @@ if __name__ == '__main__':
     print(FilterFrame.shape)
 
     distance_list = []
+    # 각 재료별 좌표를 저장하는 객체
+    sources_point_dict = { source : (sourceFrame.loc[source,'x'],
+                                     sourceFrame.loc[source,'y'],
+                                     sourceFrame.loc[source,'z']) for source in sources }
+    print(sources_point_dict.keys())
+    # 레시피와 각 재료별 거리를 저장하는 객체
+    sources_distance_dict = { source : [] for source in sources}
     for idx,row in FilterFrame.iterrows():
-        recipe_point = (row.x,row.y,row.z)
-        distance = calculate_distance(my_point,recipe_point)
-        distance_list.append((distance,row.id,row.title,recipe_point))
+        recipe_point = (row.x, row.y, row.z)
+        # 각 재료별 레시피간의 거리를 구하여 각 재료별 리스트에 저장한다.
+        for source in sources:
+            distance = calculate_distance(sources_point_dict[source],recipe_point)
+            sources_distance_dict[source].append((distance,row.id,row.title,recipe_point))
+        # distance = calculate_distance(my_point,recipe_point)
+        # distance_list.append((distance,row.id,row.title,recipe_point))
 
-    distance_list = sorted(distance_list,key=lambda x : x[0])[:50]
-    [print(i) for i in distance_list]
-
+    recommend_recipe_set = set()
+    for distance_list in sources_distance_dict.values():
+        recommend_recipe_set.update(sorted(distance_list,key=lambda x : x[0])[:10])
+    [print(i) for i in recommend_recipe_set]
+    times = time.time() - start_time
+    print(times)
     ############################### DRAW GRAPH ##################################
-    # mpl.rcParams['legend.fontsize'] = 10  # 그냥 오른쪽 위에 뜨는 글자크기 설정이다.
-    # mpl.rc('font', family='Malgun Gothic')
-    # fig = plt.figure(figsize=(20,20))  # 이건 꼭 입력해야한다.
-    # ax = fig.gca(projection='3d')
-    # theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+    mpl.rcParams['legend.fontsize'] = 10  # 그냥 오른쪽 위에 뜨는 글자크기 설정이다.
+    mpl.rc('font', family='Malgun Gothic')
+    fig = plt.figure(figsize=(20,20))  # 이건 꼭 입력해야한다.
+    ax = fig.gca(projection='3d')
+    theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
     # z = recipeFrame['z']
     # x = recipeFrame['x']
     # y = recipeFrame['y']
-    # ax.plot(x, y, z,'o', label='parametric curve')  # 위에서 정의한 x,y,z 가지고 그래프그린거다.
-    # ax.legend()  # 오른쪽 위에 나오는 글자 코드다. 이거 없애면 글자 사라진다. 없애도 좋다.
-    # for t,x,y,z in zip(recipeFrame['title'],x,y,z):
-    #     ax.text(x,y,z,t)
-    # plt.show()
+    recommend_recipe_list = list(recommend_recipe_set)
+    t,x,y,z, = [],[],[],[]
+    for i in recommend_recipe_list:
+        t.append(i[2])
+        x.append(i[3][0])
+        y.append(i[3][1])
+        z.append(i[3][2])
+
+    ax.plot(x, y, z,'o', label='recipe')  # 위에서 정의한 x,y,z 가지고 그래프그린거다.
+      # 오른쪽 위에 나오는 글자 코드다. 이거 없애면 글자 사라진다. 없애도 좋다.
+    for t,x,y,z in zip(t,x,y,z):
+        ax.text(x,y,z,t)
+
+    # 식재료 위치
+    for k,v in sources_point_dict.items():
+        ax.plot([v[0]],[v[1]],[v[2]],'o',label=k)
+        ax.text(v[0],v[1],v[2],k)
+    ax.legend()
+    plt.show()
