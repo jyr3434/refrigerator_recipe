1e75953c6d52ed28b1ee49b972a9178a96bf8e1d
briankunkel9 <briankunkel9@gmail.com>
2020-06-28 16:34:35 +0900

    edit recommend

--- Recomend/recommend_main.py
+++ Recomend/recommend_main.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     # img_array = img_array.reshape(1,224,224,3)
     # prediction.predict_label(img_array)
 
-    start_time = time.time()
+
     ############# 판별 이미지로 레시피 검색 #############
     source_path = '../../data/nlp_data/source_embedding.csv'
     recipe_path = '../../data/nlp_data/recipe_embedding.csv'
@@ -73,71 +73,49 @@ if __name__ == '__main__':
 
     sources = ['참치','고추']
     select_cat = ('볶음','일상',None,None)
-
+    select_cat = [i for i in select_cat if i is not None]
     my_point = find_point(sources)
-
-    filter1Data = []
-    for idx,row in recipeFrame.iterrows():
-        recipe_category = (row.cat1,row.cat2,row.cat3,row.cat4)
-        category_check = 0
-        select_length = 0
-        for i in range(4):
-            if select_cat[i] is not None:
-                select_length +=1
-                if select_cat[i] == recipe_category[i]:
-                    category_check +=1
-
-        # 카테고리에 해당되는지 여부
-        source_check = 0
-        if select_length == category_check :
-            recipe_sources = row.kwd_source.split('|')
-            for source in sources:
-                if source in recipe_sources:
-                    source_check += 1
-            if source_check > 0 :
-                filter1Data.append({ 'id':row.id,
-                                     'title':row.title,
-                                     'x':row.x,
-                                     'y':row.y,
-                                     'z':row.z})
-        print(select_length,category_check,source_check)
-    # for recipeFrame.iterrows()
-
-    FilterFrame = pd.DataFrame(filter1Data)
-    print(FilterFrame.shape)
-
-    distance_list = []
     # 각 재료별 좌표를 저장하는 객체
-    sources_point_dict = { source : (sourceFrame.loc[source,'x'],
-                                     sourceFrame.loc[source,'y'],
-                                     sourceFrame.loc[source,'z']) for source in sources }
+    sources_point_dict = {source: (sourceFrame.loc[source, 'x'],
+                                   sourceFrame.loc[source, 'y'],
+                                   sourceFrame.loc[source, 'z']) for source in sources}
     print(sources_point_dict.keys())
     # 레시피와 각 재료별 거리를 저장하는 객체
-    sources_distance_dict = { source : [] for source in sources}
-    for idx,row in FilterFrame.iterrows():
-        recipe_point = (row.x, row.y, row.z)
-        # 각 재료별 레시피간의 거리를 구하여 각 재료별 리스트에 저장한다.
-        for source in sources:
-            distance = calculate_distance(sources_point_dict[source],recipe_point)
-            sources_distance_dict[source].append((distance,row.id,row.title,recipe_point))
-        # distance = calculate_distance(my_point,recipe_point)
-        # distance_list.append((distance,row.id,row.title,recipe_point))
+    sources_distance_dict = {source: [] for source in sources}
+
+    select_cat_set = set(select_cat)
+    sources_set = set(sources)
+    select_length = len(select_cat_set)
+
+    start_time = time.time()
+    for idx,row in recipeFrame.iterrows():
+        recipe_category = set([row.cat1,row.cat2,row.cat3,row.cat4])
+        # 카테고리에 해당되는지
+        if len(recipe_category.intersection(select_cat_set)) == select_length:
+            # 재료 포함하고 있는지
+            recipe_sources = set(row.kwd_source.split('|'))
+            if recipe_sources.intersection(sources_set) :
+                recipe_point = (row.x, row.y, row.z)
+                # 재료별 거리 리스트 만들기 ( 거리, 아이디, 제목, 좌표 )
+                for source in sources:
+                    distance = calculate_distance(sources_point_dict[source],recipe_point )
+                    sources_distance_dict[source].append((distance, row.id, row.title, recipe_point))
+    times = time.time() - start_time
+    print(times)
 
     recommend_recipe_set = set()
     for distance_list in sources_distance_dict.values():
         recommend_recipe_set.update(sorted(distance_list,key=lambda x : x[0])[:10])
     [print(i) for i in recommend_recipe_set]
-    times = time.time() - start_time
-    print(times)
+
     ############################### DRAW GRAPH ##################################
+    plt.rc('font', size=10)  # controls default text sizes
     mpl.rcParams['legend.fontsize'] = 10  # 그냥 오른쪽 위에 뜨는 글자크기 설정이다.
+
     mpl.rc('font', family='Malgun Gothic')
     fig = plt.figure(figsize=(20,20))  # 이건 꼭 입력해야한다.
     ax = fig.gca(projection='3d')
-    theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-    # z = recipeFrame['z']
-    # x = recipeFrame['x']
-    # y = recipeFrame['y']
+
     recommend_recipe_list = list(recommend_recipe_set)
     t,x,y,z, = [],[],[],[]
     for i in recommend_recipe_list:
@@ -145,12 +123,10 @@ if __name__ == '__main__':
         x.append(i[3][0])
         y.append(i[3][1])
         z.append(i[3][2])
-
+    # 레시피 위치
     ax.plot(x, y, z,'o', label='recipe')  # 위에서 정의한 x,y,z 가지고 그래프그린거다.
-      # 오른쪽 위에 나오는 글자 코드다. 이거 없애면 글자 사라진다. 없애도 좋다.
     for t,x,y,z in zip(t,x,y,z):
         ax.text(x,y,z,t)
-
     # 식재료 위치
     for k,v in sources_point_dict.items():
         ax.plot([v[0]],[v[1]],[v[2]],'o',label=k)
